bec01318d82f46bad3cc2f02a34075671aee9711
donal <donal@github.com>
2018-03-12 14:09:26 +1100

    change dir

--- bin/mves.sh
+++ bin/mves.sh
@@ -3,11 +3,12 @@
 moving=$(ls -lrt ~/Downloads | tail -1 | rev | cut -d' ' -f1 | rev)
 
 moving="${moving#\/}"
-moving="${moving%\/}"
+
+echo $moving
 
 if [[ $moving != "github" ]]; then
   echo "moving $moving"
-  mv $moving ~/Downloads/github
+  mv $moving /Users/donal/Downloads/github
 else
   echo "trying to move the github directory into the github directory—this won't work"
   exit 1
